82c7f53aab991cce402ddf66c389f53795241f68
Opu <opuofficial24@gmail.com>
2024-06-18 23:39:03 +0600

    made a change in axios instance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://newsapi.org/v2",
   headers: {
     "X-Api-Key": import.meta.env.VITE_API_KEY,
   },
