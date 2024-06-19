afba6fde6147876d048a62313c93c897790f4700
Opu <opuofficial24@gmail.com>
2024-06-19 21:20:48 +0600

    popular news card design modified

--- src/components/popularNews/CardSkeleton.jsx
+++ src/components/popularNews/CardSkeleton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function CardSkeleton() {
   return (
-    <div className="skeleton w-100 bg-slate-50 rounded-md flex p-3 mb-3 items-start">
-      <div className="h-[45px] w-[80px] bg-slate-200 rounded-md"></div>
-      <div className="h-7 w-full ml-2 rounded-md bg-slate-200"></div>
+    <div className="skeleton w-100 bg-slate-50 rounded-md p-3 mb-3">
+      <div className="h-5 w-full mb-2 rounded-md bg-slate-200"></div>
+      <div className="h-3 w-full rounded-md bg-slate-200"></div>
     </div>
   );
 }
--- src/components/popularNews/NewsCard.jsx
+++ src/components/popularNews/NewsCard.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 
 function NewsCard({ article }) {
-  const { title, urlToImage } = article;
+  const { title } = article;
 
   if (title == "[Removed]") {
     return;
   }
 
   return (
-    <div className="rounded-md p-3 shadow-sm mb-3 flex items-start">
-      {urlToImage && (
-        <div className="rounded-sm overflow-hidden w-20 mr-2">
-          <img src={urlToImage} alt="" className="w-full" />
-        </div>
-      )}
-
+    <div className="rounded-md p-3 shadow-sm mb-3">
       <div className="text-sm font-semibold flex-1 text-slate-800">{title}</div>
     </div>
   );
