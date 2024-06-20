9cad3eb89e8f48204cccfaf200e9b800873dc089
Opu <opuofficial24@gmail.com>
2024-06-20 20:01:03 +0600

    showing total number of favorite articles

--- src/components/favoriteNews/index.jsx
+++ src/components/favoriteNews/index.jsx
@@ -16,11 +16,12 @@ function FavoriteNews() {
 
   return (
     <div className="pr-2.5">
-      <h3 className="text-2xl mb-2">Your Favorite</h3>
+      <h3 className="text-2xl mb-2">
+        Your Favorite{" "}
+        <span className="text-slate-500">({articles.length})</span>
+      </h3>
 
       <div className="favorite-news-container">
-        {/* <CardSkeleton /> */}
-        {/* <NewsCard /> */}
         {articles.map((article) => (
           <NewsCard article={article} key={article.url} />
         ))}
