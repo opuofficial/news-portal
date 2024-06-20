5eeec043aa8bd452d24cc5dbb6962dcb22a3bb45
Opu <opuofficial24@gmail.com>
2024-06-20 19:14:17 +0600

    api base url modified

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `https://${import.meta.env.VITE_SERVER_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_SERVER_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
